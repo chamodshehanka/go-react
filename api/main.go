3abb7496baf91a84854bd943cbe2de454711a81e
Shehanka <hcsperera@outlook.com>
2019-11-29 22:13:30 +0530

    go backend api done

--- api/main.go
+++ api/main.go
@@ -44,6 +44,55 @@ func getAllEvents(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(events)
 }
 
+func getOneEvent(w http.ResponseWriter, r *http.Request) {
+	// Get the ID from the url
+	eventID := mux.Vars(r)["id"]
+
+	// Get the details from an existing event
+	// Use the blank identifier to avoid creating a value that will not be used
+	for _, singleEvent := range events {
+		if singleEvent.ID == eventID {
+			json.NewEncoder(w).Encode(singleEvent)
+		}
+	}
+}
+
+func updateEvent(w http.ResponseWriter, r *http.Request) {
+	// Get the ID from the url
+	eventID := mux.Vars(r)["id"]
+	var updatedEvent event
+	// Convert r.Body into a readable formart
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintf(w, "Kindly enter data with the event title and description only in order to update")
+	}
+
+	json.Unmarshal(reqBody, &updatedEvent)
+
+	for i, singleEvent := range events {
+		if singleEvent.ID == eventID {
+			singleEvent.Title = updatedEvent.Title
+			singleEvent.Description = updatedEvent.Description
+			events = append(events[:i], singleEvent)
+			json.NewEncoder(w).Encode(singleEvent)
+		}
+	}
+}
+
+func deleteEvent(w http.ResponseWriter, r *http.Request) {
+	// Get the ID from the url
+	eventID := mux.Vars(r)["id"]
+
+	// Get the details from an existing event
+	// Use the blank identifier to avoid creating a value that will not be used
+	for i, singleEvent := range events {
+		if singleEvent.ID == eventID {
+			events = append(events[:i], events[i+1:]...)
+			fmt.Fprintf(w, "The event with ID %v has been deleted successfully", eventID)
+		}
+	}
+}
+
 func homeLink(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome back!")
 }
@@ -53,8 +102,8 @@ func main(){
 	router.HandleFunc("/", homeLink)
 	router.HandleFunc("/event", createEvent).Methods("POST")
 	router.HandleFunc("/events", getAllEvents).Methods("GET")
-	// router.HandleFunc("/events/{id}", getOneEvent).Methods("GET")
-	// router.HandleFunc("/events/{id}", updateEvent).Methods("PATCH")
-	// router.HandleFunc("/events/{id}", deleteEvent).Methods("DELETE")
+	router.HandleFunc("/events/{id}", getOneEvent).Methods("GET")
+	router.HandleFunc("/events/{id}", updateEvent).Methods("PATCH")
+	router.HandleFunc("/events/{id}", deleteEvent).Methods("DELETE")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
